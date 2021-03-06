2048b43d97fda88be2dd135eb0e6151dad32eb26
Mike Reimer <themichaelreimer@gmail.com>
2021-03-05 21:37:03 -0600

    update

---
+++ src/views/LifeTables.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="hi">
+    <br/>
+    Life Tables
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'LifeTables',
+}
+</script>
+
+<style scoped>
+#hi{
+  top:50%;
+  font-weight:lighter;
+}
+</style>
